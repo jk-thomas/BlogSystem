b2c9a6739eaea7a95c3a48e321bf49c76c87c148
Joshua Thomas <josht2999@gmail.com>
2025-07-31 14:30:16 -0500

    v.0.9.9 - comment admin / delete post fixed

--- lib/list-posts.php
+++ lib/list-posts.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Tries to delete the specified post
+ * Delete attached comments, then delete post
  * 
  * @param PDO $pdo
  * @param integer $postId
@@ -8,20 +9,31 @@
  * @throws Exception
  */
 function deletePost(PDO $pdo, $postId) {
-    $sql = "
-        DELETE FROM
+    $sqls = array(
+        // Delete comments first for foreign key
+        "DELETE FROM
+            comment
+        WHERE
+            post_id = :id",
+        "DELETE FROM
             post
         WHERE
-            id = :id
-    ";
-    $stmt = $pdo->prepare($sql);
-    if ($stmt === false) {
-        throw new Exception('There was a problem preparing this query');
-    }
-
-    $result = $stmt->execute(
-        array('id' => $postId, )
+            id = :id",
     );
+    foreach ($sqls as $sql) {
+        $stmt = $pdo->prepare($sql);
+        if ($stmt === false) {
+            throw new Exception('There was a problem preparing this query');
+        }
+
+        $result = $stmt->execute(
+            array('id' => $postId, )
+        );
+
+        if ($result === false) {
+            break;
+        }
+    }
 
     return $result !== false;
 }
